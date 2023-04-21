4a7318007c4e27b06726bb251f1cf543ce831d74
Mehmet <mehmetsahinn2002@gmail.com>
2023-04-21 21:00:13 +0300

    BMI commit

--- src/BMI.java
+++ src/BMI.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BMI {
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("kg : ");
+        int kg = sc.nextInt();
+
+        System.out.print("height as cm : ");
+        double height =  sc.nextDouble();
+
+        System.out.println(kg/(height*height));
+
+    }
 }
