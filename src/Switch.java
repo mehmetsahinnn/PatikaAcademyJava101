a70792505c810f270f2871b61b07bc71b8c71f7b
Mehmet <mehmetsahinn2002@gmail.com>
2023-04-22 23:04:34 +0300

    hesap makinesi commit

--- src/Switch.java
+++ src/Switch.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Switch {
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("İlk say : ");
+        float ilk = scanner.nextFloat();
+        System.out.print("İki say : ");
+        float iki = scanner.nextFloat();
+
+        System.out.print("Toplama için 1, Çıkarma için 2, Çarpma için 3, Bölme için 4 : ");
+        int isle = scanner.nextInt();
+
+        switch (isle){
+            case 1:
+                System.out.println(ilk+iki);
+                break;
+            case 2:
+                System.out.println(ilk-iki);
+                break;
+            case 3:
+                System.out.println(ilk*iki);
+                break;
+            case 4:
+                System.out.println(ilk/iki);
+                break;
+            default:
+                break;
+        }
+
+
+    }
 }
