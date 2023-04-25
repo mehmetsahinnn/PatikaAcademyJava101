51223cc5b459330e0654030ed479273b1e29bf22
Mehmet <mehmetsahinn2002@gmail.com>
2023-04-25 20:51:59 +0300

    faktoriyel commit

---
+++ src/whileloop/faktoriyel.java
@@ -0,0 +1,35 @@
+package whileloop;
+
+import java.util.Scanner;
+
+public class faktoriyel {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("n Giriniz : ");
+        int n = input.nextInt();
+        System.out.print("r Giriniz : ");
+        int r = input.nextInt();
+
+        int result_n = 1;
+        int result_r = 1;
+        int result_n_r = 1;
+        int difference = n-r;
+
+
+        while (n != 0 && r != 0 && difference != 0) {
+
+            result_n *= n;
+            n--;
+
+            result_r *= r;
+            r--;
+
+            result_n_r *= difference;
+            difference--;
+        }
+
+        System.out.println(result_n/(result_r*result_n_r));
+
+    }
+}
