b5e09b3fa7fb1a28d7a305a3b9c341712356a249
Mehmet <mehmetsahinn2002@gmail.com>
2023-04-25 21:36:36 +0300

    H commit

---
+++ src/whileloop/HarmonikSerisi.java
@@ -0,0 +1,17 @@
+package whileloop;
+import java.util.Scanner;
+
+public class HarmonikSerisi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        double harmonikSerisi = 0;
+        for (int i = 1; i <= n; i++) {
+            harmonikSerisi += (1.0 / i);
+        }
+
+        System.out.println(n + " sayısının harmonik serisi = " + harmonikSerisi);
+    }
+}
