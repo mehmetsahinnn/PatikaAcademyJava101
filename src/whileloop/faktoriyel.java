ed0f8c45919a445ffd34216ba24c8183080a925a
Mehmet <mehmetsahinn2002@gmail.com>
2023-04-25 20:56:15 +0300

    faktoriyel commit

--- src/whileloop/faktoriyel.java
+++ src/whileloop/faktoriyel.java
@@ -11,6 +11,11 @@ public class faktoriyel {
         System.out.print("r Giriniz : ");
         int r = input.nextInt();
 
+        if (r < 0 || r > n) {
+            System.out.println("Invalid input. n and r must be non-negative integers, and r must be less than or equal to n.");
+            return;
+        }
+
         int result_n = 1;
         int result_r = 1;
         int result_n_r = 1;
